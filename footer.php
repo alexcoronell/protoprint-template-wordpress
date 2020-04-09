4e0bfaa1a3ac1cec5c6be081b41944b8167c8a24
Alex Coronell <alexcoronell@gmail.com>
2020-04-09 14:32:30 -0500

    Se crean header.php y footer.php

---
+++ footer.php
@@ -0,0 +1,93 @@
+<footer>
+      <?php $datosEmpresa = new WP_Query('page_id=167'); ?>
+      <?php while($datosEmpresa -> have_posts()): $datosEmpresa -> the_post(); ?>
+          <article id="footer1">
+            <div class="container">
+              <div class="row">
+                <div class="col-12 col-sm-6 col-md-4 text-center text-sm-left">
+                  <h4>Contacta con nosotros</h4>
+                  <ul>
+                    <li>Telf: <?php the_field('telefono1'); ?></li>
+                    <li>Telf: <?php the_field('telefono2'); ?></li>
+                    <li><a href="mailto:<?php the_field('email'); ?>" target="_blank">Mándanos un email <br><?php the_field('email'); ?></a></li>
+                  </ul>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4 text-center text-sm-left">
+                  <h4>Dónde estamos</h4>
+                  <ul>
+                    <li><?php the_field('ubicacion1'); ?></li>
+                    <li><?php the_field('ubicacion2'); ?></li>
+                  </ul>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4 text-center text-sm-left">
+                  <h4>Horarios</h4>
+                  <ul>
+                    <li>Lunes a Viernes de <?php the_field('horario_de_lunes_a_viernes'); ?></li>
+                    <li>Sábados de <?php the_field('horario_sabados'); ?></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <?php endwhile; wp_reset_postdata(); ?>
+          </article>
+          <article id="footer2">
+          
+            <div class="container">
+              <div class="row">
+                <div class="col-12 col-md-4 text-center text-md-left">
+                  <h4>Nuestras redes sociales</h4>
+                </div>
+                <div class="col-12 col-md-4">
+                  <?php $args = array(
+                        'post_type' => 'page',
+                        'post_parent' => 180,
+                        'order' => 'ASC'
+                      );
+                      ?>
+                      <?php $redessocialescontent = new WP_Query($args); ?>
+                      
+                        <ul class="redessociales">
+                        <?php while($redessocialescontent -> have_posts()): $redessocialescontent -> the_post(); ?>
+                          <li>
+                          <?php $url = get_field('url') ?> 
+                          <?php $icono = get_field('icon') ?> 
+                            <?php  if ($url !="") { ?>
+                              <a href="<?php echo $url; ?>" target="_blank"><i class="<?php the_field('awesome_class'); ?>" aria-hidden="true" data-toggle="tooltip" data-placement="top" title=<?php the_title(); ?>></i></a>
+                            <?php } else { ?>
+                              <i class="<?php the_field('awesome_class'); ?>" aria-hidden="true" data-toggle="tooltip" data-placement="top" title=<?php the_title(); ?>></i>
+                            <?php } ?>
+                          </li>
+                        <?php endwhile; wp_reset_postdata(); ?>
+                        </ul>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4">
+                  <ul class="redessociales llamar text-center">
+                    <li><a href="tel:+573138885427"><i class="fa fa-phone-square" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="Llámanos al 313 888 5427"></i></a><span>Llámanos</span></li>
+                  </ul>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4">
+                  <ul class="redessociales whatsapp text-center">
+                    <li><a href="https://api.whatsapp.com/send?phone=573138885427&amp;text=Hola!%20Quiero%20tener%20más%20información!"><i class="fa fa-whatsapp" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="Escríbenos al Whatsapp"></i></a><span>Escríbenos al Whatsapp!!</span></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </article>
+          <article class="text-center" id="footer3">
+            <ul>
+              <li>
+                <p>&copy Impresión Digital - Todos los derechos reservados - 2020</p>
+              </li>
+            </ul>
+          </article>
+          <article class="text-center" id="footer4"><a data-scroll="data-scroll" href="#top-pagina">
+              <p>Volver arriba</p></a></article>
+          <article class="text-center" id="footer5">
+            <p>Desarrollado por Alex Coronell</p>
+          </article>
+    </footer>
+    <?php wp_footer(); ?>
+    <!-- INICIO DE WOW PARA LAS ANIMACIONES-->
+    <script type="text/javascript">new WOW().init();</script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <!-- Global site tag (gtag.js) - Google Analytics-->
+    <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-117852050-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+      dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', 'UA-117852050-1');
+    </script>
+    <!-- Google Tag Manager-->
+    <script>
+      (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+      'gtm.start': new Date().getTime(),
+      event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+      'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+      })(window, document, 'script', 'dataLayer', 'GTM-NW4HXJZ');
+    </script>
+    <!-- End Google Tag Manager--><meta name="description" content="Realizamos todo tipo de trabajos de impresión, artes gráficas, diseño digital y diseño y desarrollo web">
+<meta name="keywords" content="Impresión Digital, Diseño Web, Diseño Gráfico, Gran Formato, Diseño, Konica, Impresión Laser, Impresión Konica, Desarrollo Web, Pretroquelado, Etiquetas, Impresión, Plotter, Protoprint, Impresión centro, Impresión Belén, Belén, Centro de Medellín, Laser, Bootstrap, HTML5, CSS3, HTML, CSS, Responsive Web Design, equipos para impresion digital, plotter de impresion gran formato, impresora etiquetas, impresion de volantes, impresion volantes, publicidad, tarjetas de presentacion, volantes, impresora, flyers, tarjetas personales, impresion gran formato, publicidad imprenta digital, impresion tarjetas presentacion, impresion digital urgente, impresion facturas, negocio impresion digital, pagina web">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Impresión Digital | <?php echo "$pageName"; ?></title><link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    
+
+    <?php wp_head(); ?>
+  </head>
+  <body class="Inicio">
+    <div id="top-pagina"></div>
+    
+    <!-------------------- FONDO CARGANDO -------------------->
+    <?php
+        $args = array(
+          'post_per_page' => 1,
+          'category_name' => 'fondoCargando'
+        );
+      ?>
+      <?php $fondoCargando = new WP_Query($args); ?>
+      <?php while($fondoCargando -> have_posts()): $fondoCargando -> the_post(); ?>
+        <div id="fondo-cargando">
+          <?php the_post_thumbnail('full'); ?>
+        </div>
+      <?php endwhile; wp_reset_postdata(); ?>
+
+    
+    <!-------------------- FONDO ANIMADO DE LA PAGINA -------------------->
+    <?php
+        $args = array(
+          'post_type' => 'page',
+          'post_parent' => 51
+        );
+      ?>
+      <?php $i = 1; ?>
+      <?php $fondoAnimado = new WP_Query($args); ?>
+      <?php $count = $fondoAnimado -> found_posts(); ?>
+
+      <?php while($fondoAnimado -> have_posts()): $fondoAnimado -> the_post(); ?>
+          <div id="fondo<?php echo $i; ?>" style="background-image: url(<?php the_field('imagen'); ?>)"></div>
+      <?php $i++; endwhile; wp_reset_postdata(); ?>
+
+    <!-------------------- HEADER -------------------->
+    <header class="cabecera">
+      <div class="encabezado">
+        <div class="areamenu">
+          <div class="mostrar-menu" id="boton-menu">
+            <p><span>&#8801;</span><br/>Menú</p>
+          </div>
+          <div class="ocultar-menu ocultar-boton-menu ocultomobile" id="boton-cerrar-menu">
+            <p><span>X</span></p>
+          </div>
+          <nav class="menu menu-oculto">
+
+            <!-- Logo en el menu mobile -->
+            <?php $quienesSomos = new WP_Query('page_id=24'); ?>
+            <?php while($quienesSomos -> have_posts()): $quienesSomos -> the_post(); ?>
+              <div class="logo img-fluid">
+                <img src="<?php the_field('logo'); ?>" alt="Protoprint" title="Protoprint"/>
+              </div>
+            <?php endwhile; wp_reset_postdata(); ?>
+
+
+            <?php
+              $menu_name = 'menu_principal';
+              $locations = get_nav_menu_locations();
+              $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+              $menuitems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) );
+            ?>
+
+                  
+          <?php if( !empty( $menuitems ) ): ?>
+            <?php $i = 1; ?>
+            <?php foreach ( $menuitems as $item ): ?>
+              <div class="enlaces-menu" id="enlace<?php echo $i; ?>"><a href="<?php bloginfo('template_url'); ?><?php echo $item->url; ?>"><?php echo $item->post_title; ?></a></div>
+              <?php $i++ ?>
+            <?php endforeach; ?>
+            <?php else: // Caso contrario mostramos un mensaje indicando que el menú no tiene elementos
+    ?>
+             <p class="msg msg--error">El menú no tiene enlaces cargados.</p>
+            <?php endif; ?>
+            <div class="esconder-menu">&#215;</div>
+          </nav>
+        </div>
+        <h1>Impresión Digital</h1>
+      </div>
+    </header>
