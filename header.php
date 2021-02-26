c99df8aff54081fa47aba170473fb50d4df68df0
Alex Coronell <alexcoronell@gmail.com>
2021-02-25 20:35:17 -0500

    Actualizado title en Header.php

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Impresión Digital | <?php echo "$pageName"; ?></title><link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <title>MasterIsand | Impresión Digital<?php echo "$pageName"; ?></title><link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
     
 
     <?php wp_head(); ?>
